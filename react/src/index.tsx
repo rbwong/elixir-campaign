11a2ce1e7ddee1f29324c73598664d5cc2df2740
Ryan Wong <ryan.wong022@gmail.com>
2021-06-27 23:00:27 +1000

    Remove unused code

--- react/src/index.tsx
+++ react/src/index.tsx
@@ -1,50 +1,38 @@
-import React from "react";
-import ReactDOM from "react-dom";
+import React from "react"
+import ReactDOM from "react-dom"
 import {
   ApolloClient,
   InMemoryCache,
   ApolloProvider,
   createHttpLink,
-} from "@apollo/client";
-import { onError } from "@apollo/client/link/error";
-import { setContext } from '@apollo/client/link/context';
+} from "@apollo/client"
+import { setContext } from "@apollo/client/link/context"
 
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
-import "react-toastify/dist/ReactToastify.css";
+import "./index.css"
+import App from "./App"
+import reportWebVitals from "./reportWebVitals"
+import "react-toastify/dist/ReactToastify.css"
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/graphql",
-});
+})
 
 const authLink = setContext((_, { headers }) => {
   // get the authentication token from local storage if it exists
-  const token = localStorage.getItem('ecToken');
+  const token = localStorage.getItem("ecToken")
   // return the headers to the context so httpLink can read them
   return {
     headers: {
       ...headers,
       authorization: token ? `Bearer ${token}` : "",
-    }
+    },
   }
-});
-
-// Add your FE logging service here. e.g. Datadog
-const errorLink = onError(({ graphQLErrors, networkError }) => {
-  if (graphQLErrors)
-    graphQLErrors.map(({ message, locations, path }) =>
-      console.log(
-        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`
-      )
-    );
-  if (networkError) console.log(`[Network error]: ${networkError}`);
-});
+})
 
 const client = new ApolloClient({
-  link: authLink.concat(httpLink.concat(errorLink)),
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
-});
+})
 
 ReactDOM.render(
   <React.StrictMode>
@@ -53,9 +41,9 @@ ReactDOM.render(
     </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
-);
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+reportWebVitals()
